f4c592be861577e5a38f38cfc9b2ffc8a348536b
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-19 19:40:11 -0300

    feat: create base users migration

--- src/db/migrations/1629298969469-users.ts
+++ src/db/migrations/1629298969469-users.ts
@@ -1,7 +1,52 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-// export class users1629298969469 implements MigrationInterface {
-// public async up(queryRunner: QueryRunner): Promise<void> {}
+export class users1629298969469 implements MigrationInterface {
+  tableName = 'users';
 
-// public async down(queryRunner: QueryRunner): Promise<void> {}
-// }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          {
+            type: 'bigint',
+            name: 'id',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            type: 'varchar',
+            name: 'email',
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            type: 'varchar',
+            name: 'password',
+            isNullable: false,
+          },
+          {
+            type: 'varchar',
+            name: 'full_name',
+            isNullable: false,
+          },
+          {
+            type: 'timestamp',
+            name: 'created_at',
+            isNullable: false,
+          },
+          {
+            type: 'timestamp',
+            name: 'updated_at',
+            isNullable: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.tableName);
+  }
+}
