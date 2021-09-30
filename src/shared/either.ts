d2551c56b308ea02b1670d33f35b204e641a5f4c
henrfarias <henrique.biasibetti@gmail.com>
2021-09-29 23:15:56 -0300

    feat: create right either

--- src/shared/either.ts
+++ src/shared/either.ts
@@ -1,3 +1,21 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-classes-per-file */
+class Right<L, R> {
+  public value: R;
+
+  constructor(value: R) {
+    this.value = value;
+  }
+
+  isRight(): this is Right<L, R> {
+    return this instanceof Right;
+  }
+
+  isLeft(): this is Right<L, R> {
+    return this instanceof Left;
+  }
+}
+
 class Left<L, R> {
   public value: L;
 
@@ -8,6 +26,14 @@ class Left<L, R> {
   isLeft(): this is Left<L, R> {
     return this instanceof Left;
   }
+
+  isRight(): this is Left<L, R> {
+    return this instanceof Right;
+  }
 }
 
+export const right = <L, R>(value: R): Right<L, R> => new Right(value);
+
 export const left = <L, R>(value: L): Left<L, R> => new Left(value);
+
+export type Either<L, R> = Left<L, R> | Right<L, R>;
